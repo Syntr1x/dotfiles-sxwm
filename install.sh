374a1a6b424bd830f18eb1592e73df7c45b3174c
Maris <124806561+Syntr1x@users.noreply.github.com>
2025-05-29 18:19:46 +0200

    Update install.sh
    .bash_profile added

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ copy_configs() {
   sudo cp /home/$USER/tempconf/Rofi-themes/*.rasi /usr/share/rofi/themes
   sudo cp /home/$USER/tempconf/Ghostty-themes/* /usr/share/ghostty/themes
   sudo cp /home/$USER/tempconf/.xinitrc /home/$USER/
+  sudo cp /home/$USER/tempconf/.bash_profile /home/$USER/
   sudo cp /home/$USER/tempconf/polybar /etc/
   sudo chown -R "$USER":"$USER" ~/.config/rofi
 }
